7238bf6d2c780b53c1a7339ac1acccb717dbe0fd
ahmed2929 <mywork304@gmail.com>
2022-08-20 13:43:46 +0200

    finsish drage and drop

--- src/app.ts
+++ src/app.ts
@@ -146,8 +146,19 @@ class ProjectState extends State<Project>{
       listenerFn(this.projects.slice());
     }
   }
+  moveProject(projectId:string,newStatus:ProjectStatus){
+    const project=this.projects.find(prj=>prj.id===projectId);
+    if(project && project.status!==newStatus){
+      project.status=newStatus;
+      this.updateListeners();
+    }
 
-
+  }
+  private updateListeners(){
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
 
 }
 
@@ -241,7 +252,7 @@ class ProjectItem extends Componant<HTMLUListElement,HTMLLIElement> implements D
 }
 
 // project list class
-class ProjectList extends Componant<HTMLDivElement,HTMLElement>{
+class ProjectList extends Componant<HTMLDivElement,HTMLElement> implements DragTarget{
   
   assignedProject: Project[];
   constructor(private type: 'active'|'finished'){
@@ -252,8 +263,31 @@ class ProjectList extends Componant<HTMLDivElement,HTMLElement>{
     this.renderContent();
    
   }
+  @Autobind
+  dragOverHandler(event: DragEvent): void {
+    if(event.dataTransfer && event.dataTransfer.types[0]==='text/plain'){
+      event.preventDefault();
+      const listEl=this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+    }
 
+  }
+  @Autobind
+  dropHandler(event: DragEvent): void {
+    const prjId=event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(prjId,this.type==='active'?ProjectStatus.Active:ProjectStatus.Finished);
+  }
+  @Autobind
+  dragLeaveHandler(_: DragEvent): void {
+    const listEl=this.element.querySelector('ul')!;
+    listEl.classList.remove('droppable');
+  }
+
+  
   configure(): void {
+    this.element.addEventListener('dragover',this.dragOverHandler);
+    this.element.addEventListener('drop',this.dropHandler);
+    this.element.addEventListener('dragleave',this.dragLeaveHandler);
     projectState.addListener((projects:Project[])=>{
       const relevantProject=projects.filter(project=>{
         if(this.type==='active'){
